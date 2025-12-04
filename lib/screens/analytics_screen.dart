63982ea1bc281142de59956f9f2ec86f06c5896d
likhithapitt <123605772+likhithapitt@users.noreply.github.com>
2025-12-04 16:18:47 -0500

    Create analytics_screen.dart

---
+++ lib/screens/analytics_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsScreen extends StatelessWidget {
+  static const routeName = '/analytics';
+
+  const AnalyticsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Analytics'),
+      ),
+      body: const Center(
+        child: Text('Analytics Screen Placeholder'),
+      ),
+    );
+  }
+}
